a115ba8f1c96b327ec8ab0cb0ca5c555bbf99524
willianmendesf <robertinho1190@gmail.com>
2022-04-12 18:32:24 -0300

    update, added lombok

--- src/main/java/com/willianmendesf/bookstoremanager/entity/Author.java
+++ src/main/java/com/willianmendesf/bookstoremanager/entity/Author.java
@@ -6,11 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
-//@Data
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 	
 	@Id
--- src/main/java/com/willianmendesf/bookstoremanager/entity/Book.java
+++ src/main/java/com/willianmendesf/bookstoremanager/entity/Book.java
@@ -10,7 +10,16 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 	
 	@Id

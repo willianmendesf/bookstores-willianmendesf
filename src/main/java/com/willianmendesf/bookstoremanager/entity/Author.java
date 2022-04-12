e45de16c49ada63dea101cae8248c3a5454eb3f6
willianmendesf <robertinho1190@gmail.com>
2022-04-12 18:05:08 -0300

    added entities

---
+++ src/main/java/com/willianmendesf/bookstoremanager/entity/Author.java
@@ -0,0 +1,21 @@
+package com.willianmendesf.bookstoremanager.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer age;
+}
---
+++ src/main/java/com/willianmendesf/bookstoremanager/entity/Book.java
@@ -0,0 +1,39 @@
+package com.willianmendesf.bookstoremanager.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer pages;
+	
+	@Column(nullable = false)
+	private Integer chapters;
+	
+	@Column(nullable = false)
+	private String isbn;
+	
+	@Column(name = "publisher_name", nullable = false, unique = true)
+	private String publisherName;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+	@JoinColumn(name = "author_id")
+	private Author author;
+
+}

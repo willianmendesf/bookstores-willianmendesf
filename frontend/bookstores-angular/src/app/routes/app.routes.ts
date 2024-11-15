f90a4014b6e5e740d7e7a6d0142b68146f57ff1b
willianmendesf <5796088+willianmendesf@users.noreply.github.com>
2024-11-14 23:42:06 -0300

    increment routes, added search page, update header

--- frontend/bookstores-angular/src/app/routes/app.routes.ts
+++ frontend/bookstores-angular/src/app/routes/app.routes.ts
@@ -8,15 +8,19 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserInfoComponent } from './user-info/user-info.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'error', component: ErrorComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'user-info', component: UserInfoComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'search', component: SearchPageComponent },
   { path: 'create-account', component: CreateAccountComponent },
 ];
